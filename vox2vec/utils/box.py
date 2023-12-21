dbb1dd968b869e9f6797b1ee3bc5623c27f71416
EytanKats <eytan.kats@gmail.com>
2023-12-21 14:57:17 +0100

    enlarge bounding box if it less than patch size

--- vox2vec/utils/box.py
+++ vox2vec/utils/box.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 
-def mask_to_bbox(mask: np.ndarray) -> np.ndarray:
+def mask_to_bbox(mask: np.ndarray, min_size) -> np.ndarray:
     """
     Find the smallest box that contains all true values of the ``mask``.
     """
@@ -13,7 +13,7 @@ def mask_to_bbox(mask: np.ndarray) -> np.ndarray:
         raise ValueError('The mask is empty.')
 
     start, stop = [], []
-    for ax in itertools.combinations(range(mask.ndim), mask.ndim - 1):
+    for idx, ax in enumerate(itertools.combinations(range(mask.ndim), mask.ndim - 1)):
         nonzero = np.any(mask, axis=ax)
         if np.any(nonzero):
             left, right = np.where(nonzero)[0][[0, -1]]
@@ -21,10 +21,18 @@ def mask_to_bbox(mask: np.ndarray) -> np.ndarray:
             left, right = 0, 0
 
         # Uncomment for MRI data
-        # if right - left < 128:
-        #     offset = 128 - (right - left)
-        #     right += offset // 2 + offset % 2
-        #     left -= offset // 2
+        if right - left < min_size[2-idx]:
+            offset = 128 - (right - left)
+
+            if (left - offset//2) >= 0 or (right + offset//2 + offset%2) < mask.shape[2-idx]:
+                right += offset//2 + offset%2
+                left -= offset//2
+            elif (left - offset//2) < 0:
+                right += offset - left
+                left = 0
+            elif (right + offset//2 + offset%2) >= mask.shape[2-idx]:
+                left -= offset - (mask.shape[2-idx] - 1 - right)
+                right = mask.shape[2-idx] - 1
 
         start.insert(0, left)
         stop.insert(0, right + 1)
